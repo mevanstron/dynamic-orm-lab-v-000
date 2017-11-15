7a351d906645a762ffcd981b72bcbb766f29b7ea
Michael Evans <michael@michaelgregoryevans.com>
2017-11-15 16:54:39 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,17 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{self.table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each{|column| column_names << column["name"]}
+
+    column_names.compact
+  end
+
 end
